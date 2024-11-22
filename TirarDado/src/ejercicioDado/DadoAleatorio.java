d19ce4de375f76334fd44402934fc735758beef7
Aimar Ariznabarreta <aimar.ariznabarreta@opendeusto.es>
2024-11-22 17:23:19 +0100

    Sintetizar resulados y determinar el valor de tiro e incremetar el
    contador apropiado

--- TirarDado/src/ejercicioDado/DadoAleatorio.java
+++ TirarDado/src/ejercicioDado/DadoAleatorio.java
@@ -14,6 +14,31 @@ public class DadoAleatorio {
 	    int frecuencia6 = 0;
 	    
 	    int cara;
+	    
+		for (int tiro = 1; tiro <= 6000; tiro++) {
+			cara = 1 + (int) (Math.random() * 6);
+
+			switch (cara) {
+			case 1:
+				++frecuencia1;
+				break;
+			case 2:
+				++frecuencia2;
+				break;
+			case 3:
+				++frecuencia3;
+				break;
+			case 4:
+				++frecuencia4;
+				break;
+			case 5:
+				++frecuencia5;
+				break;
+			case 6:
+				++frecuencia6;
+				break;
+			}
+		}
 		
 	}
 	
