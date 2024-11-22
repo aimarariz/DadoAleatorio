45d5ef62b39f854ed7286a97a7f8de871180e361
Aimar Ariznabarreta <aimar.ariznabarreta@opendeusto.es>
2024-11-22 17:21:59 +0100

    Creaccion de variables

--- TirarDado/src/ejercicioDado/DadoAleatorio.java
+++ TirarDado/src/ejercicioDado/DadoAleatorio.java
@@ -1,10 +1,21 @@
 package ejercicioDado;
 
-import javax.swing.JTextArea;
+
 
 public class DadoAleatorio {
 	
-	
+	public static void main(String[] args) {
+		
+		int frecuencia1 = 0;
+		int frecuencia2 = 0;
+	    int frecuencia3 = 0;
+	    int frecuencia4 = 0;
+	    int frecuencia5 = 0;
+	    int frecuencia6 = 0;
+	    
+	    int cara;
+		
+	}
 	
 	
 	
